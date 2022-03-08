c490c1bf4253ea80c3f87e6c71a8f70cc06175d4
Rafael Martins <rmartins858@gmail.com>
2022-03-08 20:35:46 -0300

    caso o valor final seja maior que 20, imprima a a mensagem: VALOR MAIOR QUE 20

---
+++ Fundamentos/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script_3.js
@@ -0,0 +1,24 @@
+/* Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+*/
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+let media = numbers.length;
+
+let resultado = 0;
+
+for(let i = 0; i < numbers.length; i += 1 ){
+    soma = soma + numbers[i] 
+}
+
+soma = soma / media 
+
+console.log(soma);
+
+if ( soma > 20){
+    console.log( "valor maior que 20");
+}else{
+    console.log("valor menor ou igual a 20");
+}
