d2a59067c23034c2623836e42a0c71213848fa17
Rafael Martins <rmartins858@gmail.com>
2022-03-08 22:50:44 -0300

    divisao do arry

---
+++ Fundamentos/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script_9.js
@@ -0,0 +1,15 @@
+let limite = 25;
+
+let arryNumeros = [];
+
+for(let i = 1; i <= limite; i += 1 ){
+    arryNumeros.push(i);
+}
+
+console.log(arryNumeros);
+
+for(let i = 1; i <= limite; i += 1 ){
+    arryNumeros[i] = arryNumeros[i] / 2
+ }
+
+ console.log(arryNumeros);
