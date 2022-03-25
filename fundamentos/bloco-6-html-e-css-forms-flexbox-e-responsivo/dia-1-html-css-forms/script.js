fe5d2702d4a2369910894a125ca4458838aa6b77
Rafael Martins <rmartins858@gmail.com>
2022-03-25 13:09:09 -0300

    Botao limpar e Enviar

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,7 @@
+// const inter = document.querySelector('.infoheader-content')
+
+// function interrompaOcomportamento (event){
+//     event.preventDefault();
+
+// }
+// inter.addEventListener('click' , interrompaOcomportamento);
